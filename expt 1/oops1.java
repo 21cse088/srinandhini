42cd66845c15d601cfa176aac8b77065674d7932
21cse088 <111353143+21cse088@users.noreply.github.com>
2022-08-17 11:03:18 +0530

    Add files via upload

---
+++ expt 1/oops1.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class oops {
+
+    public static void main(String[] args) 
+    {
+            int a,b,result;
+            System.out.println(minimum in a list:");
+            a=95;
+            b=67;
+            int result=(a<b)?a:b;
+            system.out.println("result="+result1);
+    }
+}
